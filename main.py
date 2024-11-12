b609c7d11ca0fc2690f5c2ce930dd3f87a2322b4
Marcel Suter <marcel.suter@bzz.ch>
2024-11-12 08:12:32 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+""" the main module so pylint won't complain """
+
+def main():
+    """ the main of all mains """
+    print('This makes PyLint happy')
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_main.py
@@ -0,0 +1,3 @@
+def test_main():
+    """ Placeholder test to ensure test discovery works """
+    assert True
